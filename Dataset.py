Methods for generating datasets with repeated features, fixed the hessian trick for convex instances.

Dataset.py
@@ -14,8 +14,9 @@ class Dataset:
     def __init__(self):
         self.data = None
         self.labels = None
-        self.rng = np.random.default_rng()
+        self.rng = np.random.default_rng(111)
         self.optimal_point = None
+        self.repeated_features = False
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -24,19 +25,26 @@ class Dataset:
         return np.sum(np.log(1 + np.exp(-self.labels * np.dot(self.data, weights)))) + lambda_reg * la.norm(
             weights) ** 2
 
-    def generate_dataset(self, num_observations, num_features):
+    def generate_dataset(self, num_observations, num_features, repeated_features):
         true_weights = self.generate_true_weights(num_features)
-        self.data, self.labels = self.generate_examples(num_observations, num_features, true_weights)
+        self.data, self.labels = self.generate_examples(num_observations, num_features, true_weights, repeated_features)
         return self.data, true_weights, self.labels
 
     def set_optimal_point(self, initial_weights):
         self.optimal_point = sc.minimize(self.compute_log_loss, initial_weights).x
 
-    def generate_examples(self, num_observations, num_features, true_weights):
-        X = self.rng.standard_normal(size=(num_observations, num_features), dtype='float64')
-        X = np.c_[X, np.ones((num_observations, 1))]
-        labels = np.sign(np.dot(X, true_weights))
-        return X, labels
+    def generate_examples(self, num_observations, num_features, true_weights, repeated_features):
+        if repeated_features is not True:
+            X = self.rng.standard_normal(size=(num_observations, num_features), dtype='float64')
+            X = np.c_[X, np.ones((num_observations, 1))]
+            labels = np.sign(np.dot(X, true_weights))
+            return X, labels
+        else:
+            X = self.rng.standard_normal(size=(num_observations, int(num_features / 2)), dtype='float64')
+            X = np.c_[X, X]
+            X = np.c_[X, np.ones((num_observations, 1))]
+            labels = np.sign(np.dot(X, true_weights))
+            return X, labels
 
     def generate_true_weights(self, num_features):
         return self.rng.standard_normal(num_features + 1)
@@ -59,8 +67,8 @@ class Dataset:
             -self.labels * self.sigmoid(-self.labels * np.dot(self.data, weights + alpha * direction)) * np.dot(
                 self.data, direction))
 
-    def step_log_loss(self,alpha,weights,direction):
-        return self.compute_log_loss(weights + alpha*direction)
+    def step_log_loss(self, alpha, weights, direction):
+        return self.compute_log_loss(weights + alpha * direction)
 
     def predict(self, weights, data):
         if self.sigmoid(np.dot(weights, data.T)) >= threshold:
@@ -78,29 +86,22 @@ class Dataset:
         return good / num_examples
 
     def test_solver(self, initial_weights, solver_to_call):
-        num_iter, solution,error_array = solver_to_call(self, initial_weights)
+        num_iter, solution, error_array = solver_to_call(self, initial_weights)
         solver_name = solver_to_call.__name__
         print("Solver:", solver_name)
         print("Number of iterations:", num_iter)
-        print("Solution:", solution)
-        print("Real optimal value", self.compute_log_loss(self.optimal_point))
-        print("Optimal value found:", self.compute_log_loss(solution))
         print("Absolute error between the optimal value and solution:",
               np.abs(self.compute_log_loss(self.optimal_point) - self.compute_log_loss(solution)))
-        print("True weights:", self.optimal_point)
-        print("Percentage of good classifications:", self.test_prediction(200, self.optimal_point, solution))
+        print("Error array:", error_array)
         print("\n")
-        #iter_array = np.array(range(len(error_array)))
-        #self.plot(solver_name, iter_array, error_array)
+        # iter_array = np.array(range(len(error_array)))
+        # self.plot(solver_name + str(len(initial_weights)), iter_array, error_array)
         return error_array
 
-
-    def plot(self, file_name,x,y):
+    def plot(self, file_name, x, y):
         fig, ax = plt.subplots()
         plt.title(file_name)
-        ax.plot(x,y)
+        ax.plot(x, y)
+        ax.set_yscale('log')
         plt.grid(True)
         plt.savefig("Plot/" + file_name)
-
-
-

main.py
@@ -2,25 +2,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 from Dataset import Dataset
 from solvers import Solver
+from numpy import linalg as la
 
 
-def test_all_and_plot(num_observations=500, num_features=20):
+def adapt_array(error_array, max_iter_reached):
+    for i in range(max_iter_reached - len(error_array)):
+        error_array = np.append(error_array, error_array[-1])
+    return error_array
+
+
+def test_all_and_plot(plot_name,num_observations=500, num_features=20, repeated_features=False):
     solver = Solver()
     dataset = Dataset()
-
     # Prepare the dataset
-    X, true_weights, labels = dataset.generate_dataset(num_observations, num_features)
+    X, true_weights, labels = dataset.generate_dataset(num_observations, num_features,repeated_features)
     initial_weights = np.zeros(true_weights.shape, dtype='float64')
     dataset.set_optimal_point(initial_weights)
 
     # Tests
-
-    # dataset.test_solver(initial_weights, solver.gradient_descent)
-    # dataset.test_solver(initial_weights, solver.newton_armijo)
-    # dataset.test_solver(initial_weights, solver.standard_newton)
-    # dataset.test_solver(initial_weights, solver.gradient_descent_exact)
-    # dataset.test_solver(initial_weights, solver.greedy_newton)
-    # dataset.test_solver(initial_weights, solver.hybrid_newton)
 
     gradient_descent = dataset.test_solver(initial_weights, solver.gradient_descent)
     newton_armijo = dataset.test_solver(initial_weights, solver.newton_armijo)
@@ -28,24 +27,38 @@ def test_all_and_plot(num_observations=500, num_features=20):
     gradient_descent_exact = dataset.test_solver(initial_weights, solver.gradient_descent_exact)
     greedy_newton = dataset.test_solver(initial_weights, solver.greedy_newton)
     hybrid_newton = dataset.test_solver(initial_weights, solver.hybrid_newton)
-    x = np.arange(8)
-    # Plotting all values in the same figure
+    max_iter_reached = max([len(gradient_descent), len(newton_armijo),
+                            len(standard_newton), len(gradient_descent_exact), len(greedy_newton)
+                               , len(hybrid_newton)])
+    gradient_descent = adapt_array(gradient_descent, max_iter_reached)
+    newton_armijo = adapt_array(newton_armijo, max_iter_reached)
+    standard_newton = adapt_array(standard_newton, max_iter_reached)
+    gradient_descent_exact = adapt_array(gradient_descent_exact, max_iter_reached)
+    greedy_newton = adapt_array(greedy_newton, max_iter_reached)
+    hybrid_newton = adapt_array(hybrid_newton, max_iter_reached)
 
-    plt.plot(x, gradient_descent[:8], label='Gradient Descent')
-    plt.plot(x, newton_armijo[:8], label='Newton Armijo')
-    plt.plot(x, standard_newton[:8], label='Standard Newton')
-    plt.plot(x, gradient_descent_exact[:8], label='Gradient Descent Exact')
-    plt.plot(x, greedy_newton[:8], label='Greedy Newton')
-    plt.plot(x, hybrid_newton[:8], label='Hybrid Newton')
+    print("difference between error vectors of egd and gn:",gradient_descent_exact-greedy_newton)
+    print("precisione",np.finfo(float).eps)
+    x = np.arange(max_iter_reached)
+    # Plotting all values in the same figure
+    fig, ax = plt.subplots()
+    plt.plot(x, gradient_descent[:len(x)], label='Gradient Descent')
+    plt.plot(x, gradient_descent_exact[:len(x)], label='Gradient Descent Exact')
+    plt.plot(x, newton_armijo[:len(x)], label='Newton Armijo')
+    plt.plot(x, standard_newton[:len(x)], label='Standard Newton')
+    plt.plot(x, greedy_newton[:len(x)], label='Greedy Newton')
+    plt.plot(x, hybrid_newton[:len(x)], label='Hybrid Newton')
 
     plt.xlabel('Iteration')
     plt.ylabel('f(xk) - f*')
     plt.legend()
-    plt.title("all")
     plt.grid(True)
-
-    plt.show()
+    ax.set_yscale('log')
+    plt.savefig("Plot/" + plot_name + ".pdf")
 
 
 if __name__ == '__main__':
-    test_all_and_plot(500,20)
+    test_all_and_plot("p=20,regularized",500, 20)
+    test_all_and_plot("p=20,regularized,repeated_features",500,20,True)
+    test_all_and_plot("p=200,regularized",500,200)
+    test_all_and_plot("p=2000,regularized",500, 2000)

solvers.py
@@ -42,6 +42,10 @@ class Solver:
 
     # Standard Newton method (alpha = 1)
     def standard_newton_cholesky(self, dataset, initial_weights):
+        if len(initial_weights) - 1 == 2000 or dataset.repeated_features is True:
+            hess_trick = 1
+        else:
+            hess_trick = 0
         weights = initial_weights
         error = np.array([], dtype='float64')
         optimal_loss = dataset.compute_log_loss(dataset.optimal_point)
@@ -50,7 +54,7 @@ class Solver:
             grad = dataset.gradient(weights)
             if la.norm(grad) < eps:
                 return i,weights, error
-            hess = dataset.hessian(weights, 1)
+            hess = dataset.hessian(weights, hess_trick)
             L = la.cholesky(hess)
             w = la.solve(L, -grad)
             direction = la.solve(L.T, w)
@@ -68,15 +72,21 @@ class Solver:
         return i, weights, error
 
     def standard_newton(self, dataset, initial_weights):
+        if len(initial_weights) - 1 == 2000 or dataset.repeated_features is True:
+            hess_trick = 1
+        else:
+            hess_trick = 0
         weights = initial_weights
+        count_armijo = 0
         error = np.array([],dtype = 'float64')
         optimal_loss = dataset.compute_log_loss(dataset.optimal_point)
         error = np.append(error, dataset.compute_log_loss(initial_weights) - optimal_loss)
         for i in range(max_iter):
             grad = dataset.gradient(weights)
             if la.norm(grad) < eps:
+                print("iter e armijo steps:", i, count_armijo)
                 return i,weights,error
-            hess = dataset.hessian(weights, 1)
+            hess = dataset.hessian(weights, hess_trick)
             direction = la.solve(hess, -grad)
             new_weights = weights + direction
 
@@ -86,12 +96,14 @@ class Solver:
 
             # If the objective function has increased, compute armijo step.
             if  new_loss >= old_loss:
+                count_armijo += 1
                 step_size = self.armijo_line_search(dataset, weights, direction)
                 new_weights = weights + step_size * direction
 
             weights = new_weights
             error = np.append(error, dataset.compute_log_loss(weights) - optimal_loss)
         print("Standard newton method exceeded max iter")
+        print("iter e armijo steps:" ,i, count_armijo)
         return i, weights, error
 
     # Gradient Descent algorithm
@@ -112,9 +124,9 @@ class Solver:
 
     #Gradient descent with exact line search
     # TODO: make it not use scipy D:
-    def gradient_descent_exact(self,dataset, initial_weights):
+    def gradient_descent_exact(self, dataset, initial_weights):
         weights = initial_weights
-        error = np.array([],dtype = 'float64')
+        error = np.array([], dtype='float64')
         optimal_loss = dataset.compute_log_loss(dataset.optimal_point)
         error = np.append(error, dataset.compute_log_loss(initial_weights) - optimal_loss)
         for i in range(max_iter):
@@ -129,6 +141,10 @@ class Solver:
 
     # Newton method with armijo line search and cholesky decomposition
     def newton_armijo_cholesky(self, dataset, initial_weights):
+        if len(initial_weights) - 1 == 2000 or dataset.repeated_features is True:
+            hess_trick = 1
+        else:
+            hess_trick = 0
         weights = initial_weights
         error = np.array([],dtype = 'float64')
         optimal_loss = dataset.compute_log_loss(dataset.optimal_point)
@@ -137,7 +153,7 @@ class Solver:
             grad = dataset.gradient(weights)
             if la.norm(grad) < eps:
                 return i,weights, error
-            hess = dataset.hessian(weights)
+            hess = dataset.hessian(weights,hess_trick)
             L = la.cholesky(hess)
             w = la.solve(L, -grad)
             direction = la.solve(L.T, w)
@@ -149,6 +165,10 @@ class Solver:
 
     #Newton method with armijo line search
     def newton_armijo(self, dataset, initial_weights):
+        if len(initial_weights) - 1 == 2000 or dataset.repeated_features is True:
+            hess_trick = 1
+        else:
+            hess_trick = 0
         weights = initial_weights
         error = np.array([], dtype='float64')
         optimal_loss = dataset.compute_log_loss(dataset.optimal_point)
@@ -157,7 +177,7 @@ class Solver:
             grad = dataset.gradient(weights)
             if la.norm(grad) < eps:
                 return i,weights, error
-            hess = dataset.hessian(weights)
+            hess = dataset.hessian(weights, hess_trick)
             direction = la.solve(hess, -grad)
             step_size = self.armijo_line_search(dataset, weights, direction)
             weights = weights + step_size * direction
@@ -167,6 +187,10 @@ class Solver:
 
     #Greedy Newton method(Newton method with exact line search)
     def greedy_newton(self, dataset, initial_weights):
+        if len(initial_weights) - 1 == 2000 or dataset.repeated_features is True:
+            hess_trick = 1
+        else:
+            hess_trick = 0
         weights = initial_weights
         error = np.array([], dtype='float64')
         optimal_loss = dataset.compute_log_loss(dataset.optimal_point)
@@ -175,7 +199,7 @@ class Solver:
             grad = dataset.gradient(weights)
             if la.norm(grad) < eps:
                 return i,weights, error
-            hess = dataset.hessian(weights)
+            hess = dataset.hessian(weights, hess_trick)
             direction = la.solve(hess, -grad)
             step_size = opt.minimize_scalar(dataset.step_log_loss,args=(weights,direction)).x
             weights = weights + step_size * direction
@@ -184,6 +208,10 @@ class Solver:
         return i,weights, error
 
     def hybrid_newton(self,dataset, initial_weights):
+        if len(initial_weights) - 1 == 2000 or dataset.repeated_features is True:
+            hess_trick = 1
+        else:
+            hess_trick = 0
         weights = initial_weights
         error = np.array([], dtype='float64')
         optimal_loss = dataset.compute_log_loss(dataset.optimal_point)
@@ -192,7 +220,7 @@ class Solver:
             grad = dataset.gradient(weights)
             if la.norm(grad) < eps:
                 return i,weights, error
-            hess = dataset.hessian(weights)
+            hess = dataset.hessian(weights, hess_trick)
             newton_direction = la.solve(hess,-grad)
 
             newton_pure_step = weights + newton_direction